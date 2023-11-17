88af8900b8ee854be9abdc1308a89a29c704a5bb
Endyrm <35129304+Endyrm@users.noreply.github.com>
2023-11-17 00:30:27 -0500

    more expansions, and a segfault nightmare to deal with later.

--- index.c
+++ index.c
@@ -19,21 +19,42 @@
 
 
 int main() {
+    // Start the screen with standard options, then check if term size is correct
     InitializeScreen();
+    signal(SIGWINCH, UpdateScreen); // Screen resizer signal
 
-    signal(SIGWINCH, UpdateScreen);
-
-    for (int i = 0; i == 0;)
+    // Declare & initialize this variable to sanitize all input to prevent small \
+       screen disappearance (0 = Do not block input; 1 = Block input)
+    int isInputBlocked = VerifyMinimumScreenSize();
+    while (isInputBlocked == 1)
     {
-        printw("heythere");
-        int k = getch();
+        // Essentially check initially if the terminal is at the correct size \
+           and if not, make them verify screen size before the intro sequence begins.
+        mvwprintw(stdscr, 1, 1, "(i) Verify screen size.");
+        wrefresh(stdscr);
+    }
+    clear();
+    printw("(i) you have the minimum size requirements set.");
+    wrefresh(stdscr);
 
-        if (k == 0)
-        {
-            endwin();
-            return 0;
+
+    int ch;
+    while((ch = getch()) != '\n')
+    {
+        // Handle global shortcuts
+        switch (ch) {
+            case KEY_F(1):
+                endwin();
+                return 0;
+            case KEY_F(3):
+                printw("sample text, ");
+                break;
+            case KEY_F(12):
+                ShowLicense();
+                break;
+            default:
+                break;
         }
-        //endwin();
     }
     endwin();
     return 0;
--- interface.c
+++ interface.c
@@ -9,18 +9,39 @@ WINDOW *CreateNewWin(int height, int width, int startY, int startX);
 WINDOW *CreateNewWinBoxed(int height, int width, int startY, int startX);
 void DestroyWin(WINDOW *localWindow);
 
+// Function prototypes for the interface
+int VerifyMinimumScreenSize();
 
 
+void IntroSequence() {
 
+}
 
+// SIGWINCH handler for dynamic screen resize
 void UpdateScreen(int sig) {
+    // Refresh the screen
     endwin();
     refresh();
 
+    // Prevent early segfault
+    if (VerifyMinimumScreenSize() == 1) {
+        //mvwprintw(stdscr, 2, 1, "isInputBlocked = 1"); DEBUG
+        return;
+    } else if (VerifyMinimumScreenSize() == 0) {
+        //mvwprintw(stdscr, 2, 1, "isInputBlocked = 0"); DEBUG
+        return;
+    }
+}
+
+// Verify the minimum screen size to ensure smooth sailing
+int VerifyMinimumScreenSize()
+{
+    // Declare and initialize variables required for the resize window prompt
     int varX = 0, varY = 0, smallScrTrig = -1;
     int maxY, maxX;
     getmaxyx(stdscr, maxY, maxX);
 
+    // Determine correct spacing for best attempt centering of text
     switch (varX) { case 0 ... 9: varX = 1; break; case 10 ... 99: varX = 2; break; default: varX = 3; break;};
     switch (varY) { case 0 ... 9: varY = 1; break; case 10 ... 99: varY = 2; break; default: varY = 3; break;};
     int variableXLength = 20+varX;
@@ -30,19 +51,21 @@ void UpdateScreen(int sig) {
        Make it show the screen once the conflict is resolved, resume input afterwards.
     
 
-    if (maxX < 80 || maxY < 20) {
+    // Check if the screen is below minimums
+    if (maxX < 80 || maxY < 30) {
+        // Clear the screen
         clear();
-        WINDOW *resizeWin;
 
-        resizeWin = CreateNewWinBoxed(5, 30, (maxY / 2) - 3, (maxX - 30) / 2);
-        //int smallWin = newwin(maxY, maxX, 0, 0);
+        // Create the window, center it on the screen and place important text into it
+        WINDOW *resizeWin;
+        resizeWin = CreateNewWinBoxed(5, 30, (maxY / 2) - 2, (maxX - 30) / 2);
         mvwprintw(resizeWin, 1, 2, "Please resize your screen.");
         mvwprintw(resizeWin, 2, (5 - varX), "(Current size: %d, %d)", maxX, maxY);
-        mvwprintw(resizeWin, 3, 4, "(Minimum size: 80, 20)");
-        wrefresh(resizeWin); // Refresh the screen
-    }
-
-    
+        mvwprintw(resizeWin, 3, 4, "(Minimum size: 80, 30)");
+        wrefresh(resizeWin); 
+        return 1;
+    } 
+    return 0;
 }
 
 // This starts the screen function, along with required options
@@ -56,6 +79,43 @@ void InitializeScreen() {
     keypad(stdscr, TRUE);
 }
 
+// There is likely a smarter way to do this, but I have limited time. \
+   This function prints out the MIT license on screen.
+void ShowLicense() {
+    int maxY, maxX;
+    getmaxyx(stdscr, maxY, maxX);
+
+    WINDOW *licenseWindow;
+    licenseWindow = CreateNewWinBoxed(30, 80, (maxY / 2) - 15, (maxX - 80) / 2);
+
+    // Print out the MIT license
+    mvwprintw(licenseWindow, 1, 1, "Copyright (c) 2023 Lexi Charron");
+
+    mvwprintw(licenseWindow, 3, 1, "Permission is hereby granted, free of charge, to any person obtaining a copy");
+    mvwprintw(licenseWindow, 4, 1, "of this software and associated documentation files (the \"Software\"), to deal");
+    mvwprintw(licenseWindow, 5, 1, "in the Software without restriction, including without limitation the rights");
+    mvwprintw(licenseWindow, 6, 1, "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell");
+    mvwprintw(licenseWindow, 7, 1, "copies of the Software, and to permit persons to whom the Software is");
+    mvwprintw(licenseWindow, 8, 1, "furnished to do so, subject to the following conditions:");
+
+    mvwprintw(licenseWindow, 10, 1, "The above copyright notice and this permission notice shall be included in all");
+    mvwprintw(licenseWindow, 11, 1, "copies or substantial portions of the Software.");
+
+    mvwprintw(licenseWindow, 13, 1, "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
+    mvwprintw(licenseWindow, 14, 1, "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
+    mvwprintw(licenseWindow, 15, 1, "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE");
+    mvwprintw(licenseWindow, 16, 1, "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
+    mvwprintw(licenseWindow, 17, 1, "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
+    mvwprintw(licenseWindow, 18, 1, "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
+    mvwprintw(licenseWindow, 19, 1, "SOFTWARE.");
+
+    // Tell the user how to exit this prompt, then exit.
+    mvwprintw(licenseWindow, 29, 20, " (i): Press any key to exit this prompt ");
+    wrefresh(licenseWindow);
+    getch();
+    DestroyWin(licenseWindow);
+    clear();
+}
 
 
 
@@ -80,4 +140,11 @@ WINDOW *CreateNewWinBoxed(int height, int width, int startY, int startX)
     wrefresh(localWindow);
 
     return localWindow;
+}
+
+void DestroyWin(WINDOW *localWindow) {
+    // Remove the border, refresh the window and then destroy it.
+    wborder(localWindow, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    wrefresh(localWindow);
+    delwin(localWindow);
 }
